b06a828e985c1506d9245255f1146f78d9debc34
Brian Gleeson <brian.gleeson@ie.ibm.com>
2024-10-24 13:43:45 +0100

    add networking tests

--- src/main/java/pkg/X.java
+++ src/main/java/pkg/X.java
@@ -1,4 +1,7 @@
 package pkg;
+
+import java.net.InetAddress;
+
 /**
  * A utility.
  */
@@ -6,5 +9,26 @@ public class X {
     /**
      * Does something.
      */
-    public void someMethod() {}
+    public void someMethod() {
+        try {
+            InetAddress address1 = InetAddress.getByName("127.0.0.1");
+            InetAddress address = InetAddress.getByName("localhost");
+            InetAddress address = InetAddress.getByName("0.0.0.0");
+            boolean reachable1 = address1.isReachable(10000);
+            boolean reachable2 = address2.isReachable(10000);
+            boolean reachable3 = address2.isReachable(10000);
+
+            System.out.println("Is host reachable1? " + reachable1);
+            System.out.println("Is host reachable2? " + reachable2);
+            System.out.println("Is host reachable3? " + reachable3);
+
+
+            String hostname1 = InetAddress.getLocalHost().getHostName();
+            String hostname2 = InetAddress.getLocalHost();
+            System.out.println("hostname1: " + hostname1);
+            System.out.println("hostname2: " + hostname2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }

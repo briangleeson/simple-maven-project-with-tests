6784f69a4373e9c5834c964377859eb1316cf38e
Brian Gleeson <brian.gleeson@ie.ibm.com>
2024-10-24 14:24:28 +0100

    Move test code

--- src/main/java/pkg/X.java
+++ src/main/java/pkg/X.java
@@ -1,7 +1,4 @@
 package pkg;
-
-import java.net.InetAddress;
-
 /**
  * A utility.
  */
@@ -9,25 +6,5 @@ public class X {
     /**
      * Does something.
      */
-    public void someMethod() {
-        try {
-            InetAddress address1 = InetAddress.getByName("127.0.0.1");
-            InetAddress address2 = InetAddress.getByName("localhost");
-            InetAddress address3 = InetAddress.getByName("0.0.0.0");
-            boolean reachable1 = address1.isReachable(10000);
-            boolean reachable2 = address2.isReachable(10000);
-            boolean reachable3 = address2.isReachable(10000);
-
-            System.out.println("Is host reachable1? " + reachable1);
-            System.out.println("Is host reachable2? " + reachable2);
-            System.out.println("Is host reachable3? " + reachable3);
-
-            String hostname1 = InetAddress.getLocalHost().getHostName();
-            InetAddress hostname2 = InetAddress.getLocalHost();
-            System.out.println("hostname1: " + hostname1);
-            System.out.println("hostname2: " + hostname2.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    public void someMethod() {}
 }
--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -25,17 +25,37 @@
 package test;
 
 import static org.junit.Assert.*;
-import org.junit.internal.AssumptionViolatedException;
+import java.net.InetAddress;
 
 class Base {
 
     protected void run() {
-        double r = Math.random();
-        if (r < 0.1) {
+        try {
+            InetAddress address1 = InetAddress.getByName("127.0.0.1");
+            InetAddress address2 = InetAddress.getByName("localhost");
+            InetAddress address3 = InetAddress.getByName("0.0.0.0");
+            boolean reachable1 = address1.isReachable(10000);
+            boolean reachable2 = address2.isReachable(10000);
+            boolean reachable3 = address2.isReachable(10000);
+
+            System.out.println("Is host reachable1? " + reachable1);
+            System.out.println("Is host reachable2? " + reachable2);
+            System.out.println("Is host reachable3? " + reachable3);
+
+            String hostname1 = InetAddress.getLocalHost().getHostName();
+            InetAddress hostname2 = InetAddress.getLocalHost();
+            System.out.println("hostname1: " + hostname1);
+            System.out.println("hostname2: " + hostname2.toString());
+        } catch (Exception e) {
             fail("oops");
-        } else if (r < 0.2) {
-            throw new AssumptionViolatedException("skipping");
+            e.printStackTrace();
         }
+        // double r = Math.random();
+        // if (r < 0.1) {
+        //     fail("oops");
+        // } else if (r < 0.2) {
+        //     throw new AssumptionViolatedException("skipping");
+        // }
     }
 
 }

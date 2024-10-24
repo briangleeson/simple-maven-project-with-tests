aa1ed7deb0fdab6974e1b7be717ac15db7a7bb43
Brian Gleeson <brian.gleeson@ie.ibm.com>
2024-10-24 13:46:54 +0100

    Fix bug

--- src/main/java/pkg/X.java
+++ src/main/java/pkg/X.java
@@ -12,8 +12,8 @@ public class X {
     public void someMethod() {
         try {
             InetAddress address1 = InetAddress.getByName("127.0.0.1");
-            InetAddress address = InetAddress.getByName("localhost");
-            InetAddress address = InetAddress.getByName("0.0.0.0");
+            InetAddress address2 = InetAddress.getByName("localhost");
+            InetAddress address3 = InetAddress.getByName("0.0.0.0");
             boolean reachable1 = address1.isReachable(10000);
             boolean reachable2 = address2.isReachable(10000);
             boolean reachable3 = address2.isReachable(10000);
@@ -21,7 +21,6 @@ public class X {
             System.out.println("Is host reachable1? " + reachable1);
             System.out.println("Is host reachable2? " + reachable2);
             System.out.println("Is host reachable3? " + reachable3);
-
 
             String hostname1 = InetAddress.getLocalHost().getHostName();
             String hostname2 = InetAddress.getLocalHost();
